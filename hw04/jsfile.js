4d1808696345ed249544973fa6ee5fb8361eed08
Денис Сахаров <sakhirsp@gmail.com>
2023-04-07 16:58:54 +0300

    commit

---
+++ hw04/jsfile.js
@@ -0,0 +1,73 @@
+class TreeNode{
+    constructor(value){
+        this.value=value
+        this.left=undefined
+        this.right=undefined
+    }
+    add(value){
+        if(this.value === value) return
+        if(value<this.value){
+            this.addLeft(value)
+        }
+        else{
+            this.addRight(value)
+        }
+    }
+    addLeft(value){
+        if(this.left){
+            return this.left.add(value)
+        }
+        this.left=new TreeNode(value)
+    }
+    addRight(value){
+        if(this.right){
+            return this.right.add(value)
+        }
+        this.right=new TreeNode(value)
+    }
+}
+
+class BinaryTree{
+    constructor(){
+        this.root=undefined
+    }
+    add(value){
+        if(this.root){
+            return this.root.add(value)
+        }
+       this.root = new TreeNode(value)
+    }
+    find(value){
+        if(!this.root) return undefined
+        let current = this.root
+        while(true){
+            if(current === undefined) return console.log("no such element in a tree ")
+            if(current.value === value){
+                console.log("found  " )
+                return current
+            }
+            if(current.value > value){
+                current =current.left
+            }
+            else{
+                current=current.right
+            }
+        }
+    }
+}
+
+const tree = new BinaryTree();
+
+tree.add(29)
+tree.add(13)
+tree.add(17)
+tree.add(23)
+tree.add(5)
+tree.add(15)
+tree.add(37)
+tree.add(31)
+tree.add(41)
+//console.log(JSON.stringify(tree,null,"  "))
+
+const res = tree.find(15)
+console.log(res)
---
+++ hw04/jsfilegraf.js
@@ -0,0 +1,132 @@
+const users = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const relationships = [
+	[0, 3],
+	[1, 3],
+	[1, 4],
+	[2, 5],
+	[3, 6],
+	[4, 6],
+	[4, 7],
+	[4, 10],
+	[5, 7],
+	[6, 8],
+	[6, 10],
+	[7, 8],
+	[7, 9],
+	[8, 10],
+	[10, 11],
+];
+
+const matrix = getMatrix(users, relationships);
+
+console.log(matrix);
+console.log(getFriends(matrix, 8, 2));
+
+function getFriends(matrix, 
+    ctrlIndex, //какой элемент начинаем смотреть
+    round) //какой уровень связей смотрим
+    {
+	if (round === 0) {
+		return;
+	}
+
+	const friends = [];
+
+	for (let index = 0; index < matrix[ctrlIndex].length; index++) {
+		if (matrix[ctrlIndex][index]) {
+			friends.push(index);
+		}
+	}
+
+	if (round === 1) {
+		return friends;
+	}
+
+	if (round === 2) {
+		let friends2 = [];
+
+		for (const friend of friends) {
+			friends2.push(...getFriends(matrix, friend, 1));
+		}
+
+		friends2 = friends2.filter((friend) => {
+			if (friend === ctrlIndex) {
+				return false;
+			}
+
+			if (friends.includes(friend)) {
+				return false;
+			}
+
+			return true;
+		});
+
+		friends2 = [...new Set(friends2)];
+
+		return friends2;
+	}
+}
+
+function getMatrix(nodes, edges) {
+	const matrix = [];
+
+	for (let i = 0; i < nodes.length; i++) {
+		const row = [];
+
+		for (let j = 0; j < nodes.length; j++) {
+			row.push(0);
+		}
+
+		matrix.push(row);
+	}
+
+	for (const [a, b] of edges) {
+		matrix[a][b] = 1;
+		matrix[b][a] = 1;
+	}
+
+	return matrix;
+}
+
+console.log(matrix);
+
+searchByHeight(matrix, 8, (x) => {
+	console.log(x);
+});
+
+function searchByWidth(matrix, ctrlIndex, callback) {
+	const checked = [];
+	const forCheck = [ctrlIndex];
+
+	while (forCheck.length) {
+		const node = forCheck.shift();
+		checked.push(node);
+
+		callback(node);
+
+		for (let i = 0; i < matrix[node].length; i++) {
+			const friend = matrix[node][i];
+
+			if (friend && !checked.includes(i) && !forCheck.includes(i)) {
+				forCheck.push(i);
+			}
+		}
+	}
+}
+
+function searchByHeight(matrix, ctrlIndex, callback) {
+	const checked = [];
+
+	searchByHeightMaster(ctrlIndex);
+
+	function searchByHeightMaster(currentIndex) {
+		checked.push(currentIndex);
+		callback(currentIndex);
+
+		for (let i = 0; i < matrix[currentIndex].length; i++) {
+			if (matrix[currentIndex][i] && !checked.includes(i)) {
+				searchByHeightMaster(i);
+			}
+		}
+	}
+}
